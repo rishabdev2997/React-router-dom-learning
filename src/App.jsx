5db1be37b83a8e989ae55f6f9302c1240917ef81
rishab <rishabstudent2000@gmail.com>
2024-11-04 12:46:14 +0530

    React router dom

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+  return <div>
+    <BrowserRouter>
+    <Routes>
+      <Route></Route>
+      <Route></Route>
+    </Routes>
+    </BrowserRouter>
+  </div>
+    
+      
+
+}
+
+export default App
